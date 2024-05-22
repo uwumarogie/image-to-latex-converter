e35eea27d169bbce7b8ec33c636b4e602aae2844
phillipsuwumarogie <phillips.uwumarogie@tum.de>
2024-05-22 17:42:03 +0200

    chpre: merge main into head and format code

--- src/app/_components/drag-and-drop-area.tsx
+++ src/app/_components/drag-and-drop-area.tsx
@@ -1,47 +1,47 @@
 import React, { useState } from "react";
 
-type DragAndDropArea=  {
-    onDrop: (file: File | null) => void;
-    children: React.ReactNode;
-}
+type DragAndDropArea = {
+  onDrop: (file: File | null) => void;
+  children: React.ReactNode;
+};
 
-export function DragAndDropArea({ onDrop, children }: DragAndDropArea ) {
-    const [dragActive, setDragActive] = useState(false);
-    type DragEvent = React.DragEvent<HTMLDivElement>
-    const handleDragEnter = (e: DragEvent) => {
-        e.preventDefault();
-        e.stopPropagation();
-        setDragActive(true);
-    };
+export function DragAndDropArea({ onDrop, children }: DragAndDropArea) {
+  const [dragActive, setDragActive] = useState(false);
+  type DragEvent = React.DragEvent<HTMLDivElement>;
+  const handleDragEnter = (e: DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(true);
+  };
 
-    const handleDragLeave = (e: DragEvent) => {
-        e.preventDefault();
-        e.stopPropagation();
-        setDragActive(false);
-    };
+  const handleDragLeave = (e: DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(false);
+  };
 
-    const handleDragOver = (e: DragEvent) => {
-        e.preventDefault();
-        e.stopPropagation();
-    };
+  const handleDragOver = (e: DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
 
-    const handleDrop = (e: DragEvent) => {
-        e.preventDefault();
-        e.stopPropagation();
-        const file = e.dataTransfer.files[0];
-        onDrop(file);
-        setDragActive(false);
-    };
+  const handleDrop = (e: DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const file = e.dataTransfer.files[0];
+    onDrop(file);
+    setDragActive(false);
+  };
 
-    return (
-        <div
-            onDragEnter={handleDragEnter}
-            onDragLeave={handleDragLeave}
-            onDragOver={handleDragOver}
-            onDrop={handleDrop}
-            className={`bg-white flex flex-col justify-center items-center rounded-xl p-5 border border-black ${dragActive ? "border-dashed" : ""}`}
-        >
-            {children}
-        </div>
-    );
+  return (
+    <div
+      onDragEnter={handleDragEnter}
+      onDragLeave={handleDragLeave}
+      onDragOver={handleDragOver}
+      onDrop={handleDrop}
+      className={`bg-white flex flex-col justify-center items-center rounded-xl p-5 border border-black ${dragActive ? "border-dashed" : ""}`}
+    >
+      {children}
+    </div>
+  );
 }
